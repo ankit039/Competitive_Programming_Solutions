df49039073d6579192afe76477bc37666c819041
Kaushal Kumar Agarwal <kaushalag29@gmail.com>
2019-10-04 10:27:17 +0530

    Delete wrong location

--- CIRCLEDIV.py
+++
@@ -1,9 +0,0 @@
-#Your code goes here Python 2
-t=input()
-cas=1
-mod=1000000007
-while(t>0):
-    t=t-1
-    n=input()
-    print "Case "+str(cas)+": "+str(((n*(n-1)/2)%mod+(n*(n-1)*(n-2)*(n-3)/24)%mod+1)%mod)
-    cas=cas+1
