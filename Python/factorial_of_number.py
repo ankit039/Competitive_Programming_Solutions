d15e91afd12cfa148244d78623f3cc91f80c438c
JinapriyaMadushanka <50899862+JinapriyaMadushanka@users.noreply.github.com>
2019-10-06 07:30:06 +0530

    Create factorial_of_number.py

---
+++ Python/factorial_of_number.py
@@ -0,0 +1,11 @@
+def factorial(num):
+    fact =1
+    if num<0:
+        print("Enter a valid number")
+    elif num==0:
+        print("1")
+    else:
+        for i in range(1,num+1):
+            fact = fact * i
+        print(fact)
+    
