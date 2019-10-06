b2ee2da7a55ef8c6ae2c4175acd43d657839dc27
gamino33 <45882098+gamino33@users.noreply.github.com>
2019-10-06 22:58:03 +0900

    Update quicksort.cpp

--- C++/quicksort.cpp
+++ C++/quicksort.cpp
@@ -1,38 +1,54 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <stdio.h>
 
-template<typename T>
-void qsfunc(vector<T> &array, int left, int right){
-  if(left >= right) return;
-  int p = array[left];
-
-  int l = left, r = right;
-  while(1){
-    while(l < right and array[l] < p) l++;
-    while(r > left and array[r] > p) r--;
-    if(l >= r) break;
-    swap(array[l], array[r]);
-  }
-
-  qsfunc(array, left, l-1);
-  qsfunc(array, l+1, right);
+void swap(int a, int b)
+{
+    int t = a;
+    a = b;
+    b = t;
 }
 
-template<typename T>
-void quicksort(vector<T> &array){
-  int n = array.size();
-  qsfunc(array, 0, n-1);
+int partition (int array[], int l, int r) {
+    int pivot = array[r];
+    int i = (l - 1);
+
+    for (int j = l; j <= r - 1; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[r]);
+    return (i + 1);
 }
 
-int main() 
-{ 
-  srand((unsigned int)time(NULL));
-  vector<int> arr;
-  for(int i = 0; i < 10; i++) arr.emplace_back(rand()%1000);
-  quicksort(arr);
-  for(int i = 0; i < arr.size(); i++){
-    cout << arr[i] << " ";
-  }
-  cout << endl;
-	return 0; 
-} 
+void quickSort(int array[], int l, int r) {
+    if (l < r) {
+        int pivot = partition(array, l, r);
+        quickSort(array, l, pivot - 1);
+        quickSort(array, pivot + 1, r);
+    }
+}
+
+
+
+int main (void) {
+
+    int i;
+    int array[10] = {3, 6, 1, 7, 2, 0, 4, 5, 9, 8};
+
+    printf("       array: ");
+    for (i = 0; i < sizeof(array) / sizeof(array[0]); i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    quickSort(array, 0, sizeof(array) / sizeof(array[0]) - 1);
+
+    printf("sorted array: ");
+    for (i = 0; i < sizeof(array) / sizeof(array[0]); i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
