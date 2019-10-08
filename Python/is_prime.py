5a52d6b9b625601b26db94f5d5e400b5358c437d
Beatriz <beatrizalice00@gmail.com>
2019-10-08 20:45:18 -0300

    refactoring condition for prime"

--- Python/is_prime.py
+++ Python/is_prime.py
@@ -1,9 +1,9 @@
 import math
 
 def is_prime(number):
-  if number <= 1: return False
+  if ((number <= 1) or((number % 2) == 0)): return False
   if number == 2: return True
-  if (number % 2) == 0: return False
+  
 
   for i in range(3, int(math.sqrt(number)) +1,2):
     if number % 1 == 0: return False
