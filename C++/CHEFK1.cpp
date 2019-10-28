ee0e87b587b64fff551ab7ff233b15cf361518fb
ABHISHEK AMRUTE <41743655+ABHISHEK-AMRUTE@users.noreply.github.com>
2019-10-28 15:20:48 +0530

    Update CHEFK1.cpp

--- C++/CHEFK1.cpp
+++ C++/CHEFK1.cpp
@@ -1,84 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ine 9999999
+unsigned long long int cal(unsigned long long int x)
+{
+    x= (x*(x+1))/2;
+    return x;
+}
 int main()
 {
-
-    int d[5][5]={0,ine,ine,ine,ine,
-                 ine,0,ine,ine,ine,
-                 ine,ine,0,ine,ine,
-                 ine,ine,ine,0,ine,
-                 ine,ine,ine,ine,0};
-    fstream file;
-    file.open("edge.txt",ios::in);
-    int a,b,c;
-    while(file>>a>>b>>c)
-    {   cout<<a<<" "<<b<<" "<<c<<endl;
-        d[a-1][b-1]=c;
-    }
-    int p[5][5];
-
-    //*** initializing predessesor matrix **********
-    for(int x=0;x<5;x++)
+    int  t;
+    cin>>t;
+    while(t--)
     {
-        for(int y=0;y<5;y++)
-        {
-
-            if(d[x][y]==0||d[x][y]==ine)p[x][y]=0;
-            else p[x][y]=x+1;
-        }
-    }
-
-//main floyed warshell algorithm
- for(int x=0;x<5;x++)
-    {
-        for(int y=0;y<5;y++)
-        {
-
-            for(int z=0;z<5;z++)
-            {
-
-                if(d[y][z]>d[y][x]+d[x][z])
-                {
-                  d[y][z]=d[y][x]+d[x][z] ;
-                  p[y][z]=p[x][z];
-                }
-            }
-        }
-
-
-        cout<<"D matrix("<<x+1<<") is : \n";
-        for(int y=0;y<5;y++)
-        {
-
-            for(int z=0;z<5;z++)
-            {
-
-               if(d[y][z]==ine)
-                cout<<"INF ";
-               else
-               cout<<" "<<d[y][z]<<" ";
-            }
-           cout<<endl;
+       int n,k;
+       cin>>n>>k;
+       int a[n];
+       for(int x=0;x<n;x++)
+       cin>>a[x];
+       unsigned long long int sum=0;
+       for(int x=0;x<n;x++)
+       {
+           for(int y=0;y<n;y++)
+           {
+               if(y!=x)
+               {
+                   if(y<x)
+                   {
+                           if(a[y]<a[x])sum+=cal(k-1);
+                   }
+                   else
+                   {if(a[y]<a[x])sum+=cal(k);
+                   }
+                   
+               }
+           }
        }
-
-
-        cout<<"Predesessor matrix("<<x+1<<") is : \n";
-
-         for(int y=0;y<5;y++)
-        {
-
-            for(int z=0;z<5;z++)
-            {
-
-
-              cout<<p[y][z]<<" ";
-                          }
-
-         cout<<endl;
-        }
+       cout<<sum<<endl;
     }
-
-
-
 }
